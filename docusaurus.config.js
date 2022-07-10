d7d4bfac86e495191be7748b10785129eabf20bf
Razgriz <47901762+rrazgriz@users.noreply.github.com>
2022-07-10 17:47:30 -0400

    Remove "Edit This Page" link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,7 +25,7 @@ const config = {
           routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
           showLastUpdateTime: true,
         },
         blog: false,
