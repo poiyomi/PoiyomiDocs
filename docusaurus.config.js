8b86eff660fbc7ba42954245ccbe8aef7a53ebe9
Razgriz <47901762+rrazgriz@users.noreply.github.com>
2022-07-10 17:51:31 -0400

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -28,15 +28,15 @@ const config = {
           // editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
           showLastUpdateTime: true,
         },
-        blog: true,
-        /*
+        // blog: true,
+        
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // editUrl:
+            // 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
-        */
+        
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
