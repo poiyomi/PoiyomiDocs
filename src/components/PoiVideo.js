0b1fbed0e89f86ccd48fb3f2cb4e0bf60c80e185
BluWizard10 <17975114+BluWizard10@users.noreply.github.com>
2025-09-28 06:28:27 -0700

    Revert

--- src/components/PoiVideo.js
+++ src/components/PoiVideo.js
@@ -3,6 +3,6 @@ import ReactPlayer from "react-player"
 
 export default function PoiVideo({url}) {
 	return (
-		<ReactPlayer width="100%" height="100%" playing controls="false" muted='true' loop='true' playsinline='true' src={url} />
+		<ReactPlayer width="100%" height="100%" playing="true" controls="false" muted="true" loop="true" playsinline="true" src={url} />
 	);
 }
