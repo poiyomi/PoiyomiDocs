53c051fb01746fe12b56e82079e128ae92a90d36
BluWizard10 <17975114+BluWizard10@users.noreply.github.com>
2025-09-28 06:13:51 -0700

    Fallback to native web controls

--- src/components/PoiVideo.js
+++ src/components/PoiVideo.js
@@ -1,28 +1,9 @@
 import React from "react"
 import ReactPlayer from "react-player"
-import {
-	MediaController,
-	MediaControlBar,
-	MediaTimeRange,
-	MediaTimeDisplay,
-	MediaVolumeRange,
-	MediaPlaybackRateButton,
-	MediaPlayButton,
-	MediaSeekBackwardButton,
-	MediaSeekForwardButton,
-	MediaMuteButton,
-	MediaFullscreenButton,
-} from "media-chrome/react";
 
 export default function PoiVideo({url}) {
 	return (
-		<MediaController 
-			style={{
-				width: "100%",
-				aspectRatio: "16/9"
-			}}
-		><ReactPlayer
-			slot="media"
+		<ReactPlayer
 			src={url} 
 			controls={false}
 			muted={true}
@@ -32,18 +13,7 @@ export default function PoiVideo({url}) {
 			style={{
 				width: "100%", 
 				height: "100%",
-				playsinline: "true",
-				"--controls": "none",
 			}}
 		></ReactPlayer>
-		<MediaControlBar>
-			<MediaPlayButton />
-			<MediaTimeRange />
-			<MediaTimeDisplay showDuration />
-			<MediaMuteButton />
-			<MediaVolumeRange />
-			<MediaFullscreenButton />
-		</MediaControlBar>
-		</MediaController>
 	);
 }
