08986466c3ce6eaf9c2445d3fe54901b6c097a07
BluWizard10 <17975114+BluWizard10@users.noreply.github.com>
2025-09-28 05:51:53 -0700

    Upgraded react-player and added media-chrome
    Upgrade to Docusaurus 3.9.1

--- src/components/PoiVideo.js
+++ src/components/PoiVideo.js
@@ -1,8 +1,48 @@
-import React from 'react'
-import ReactPlayer from 'react-player'
+import React from "react"
+import ReactPlayer from "react-player"
+import {
+	MediaController,
+	MediaControlBar,
+	MediaTimeRange,
+	MediaTimeDisplay,
+	MediaVolumeRange,
+	MediaPlaybackRateButton,
+	MediaPlayButton,
+	MediaSeekBackwardButton,
+	MediaSeekForwardButton,
+	MediaMuteButton,
+	MediaFullscreenButton,
+} from "media-chrome/react";
 
 export default function PoiVideo({url}) {
 	return (
-		<ReactPlayer width="100%" height="100%" playing controls="false" muted loop='true' playsinline='true' url={url} />
+		<MediaController 
+			style={{
+				width: "100%",
+				aspectRatio: "16/9"
+			}}
+		><ReactPlayer
+			slot="media"
+			src={url} 
+			controls={false}
+			muted="true"
+			playing={true}
+			playsInLine="true"
+			loop="true"
+			style={{
+				width: "100%", 
+				height: "100%",
+				"--controls": "none",
+			}}
+		></ReactPlayer>
+		<MediaControlBar>
+			<MediaPlayButton />
+			<MediaTimeRange />
+			<MediaTimeDisplay showDuration />
+			<MediaMuteButton />
+			<MediaVolumeRange />
+			<MediaFullscreenButton />
+		</MediaControlBar>
+		</MediaController>
 	);
 }
