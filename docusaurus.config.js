bb6fa5c0f7533773b851a5903386e65221f482de
BluWizard10 <17975114+BluWizard10@users.noreply.github.com>
2026-03-14 20:18:45 -0700

    Revert "Added i18n assets for ES, DE, JA, KO, and ZH."
    This reverts commit 424f815afeca08e8d4adc877939dcdf44b85a011.
    
    It seems that the more locales are added, the more memory consumes on Deployment, which will cost money.
    
    Revisit this later when it becomes relevant.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -27,25 +27,10 @@ export default {
 
   i18n: {
     defaultLocale: 'en',
-    locales: ['en', 'es', 'de', 'ja', 'ko', 'zh', 'ru'],
+    locales: ['en', 'ru'],
     localeConfigs: {
       en: {
         htmlLang: 'en-US',
-      },
-      es: {
-        htmlLang: 'es-MX',
-      },
-      de: {
-        htmlLang: 'de-DE',
-      },
-      ja: {
-        htmlLang: 'ja-JP',
-      },
-      ko: {
-        htmlLang: 'ko-KR',
-      },
-      zh: {
-        htmlLang: 'zh-CN',
       },
       ru: {
         htmlLang: 'ru-RU',
