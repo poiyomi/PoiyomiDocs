cc3567062f7da244470c82f75db651b85e184960
BluWizard10 <17975114+BluWizard10@users.noreply.github.com>
2026-03-25 23:13:19 -0700

    Update packages.

--- src/theme/Root.tsx
+++ src/theme/Root.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import RefModal from '@site/src/components/RefModal';
 
 export default function Root({children}: {children: React.ReactNode}) {
@@ -6,6 +7,7 @@ export default function Root({children}: {children: React.ReactNode}) {
     <>
       <RefModal />
       {children}
+      <SpeedInsights />
     </>
   );
 }
